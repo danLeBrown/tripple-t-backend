9dbea8a36c431c34a49665fa74737aeb23702405
Ayomide Ojo <ayomidedaniel00@gmail.com>
2025-12-22 16:29:06 +0100

    chore: delete lead module

--- src/domains/analytics/analytics.module.ts
+++ src/domains/analytics/analytics.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../auth/auth.module';
-import { LeadsModule } from '../leads/leads.module';
 import { SharedModule } from '../shared/shared.module';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
 
 @Module({
-  imports: [LeadsModule, AuthModule, SharedModule],
+  imports: [AuthModule, SharedModule],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
   exports: [],
